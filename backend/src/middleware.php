33f56ff5451f58d105af01054b78b4767221bd70
Aleksej Kamynin <zendzirou@gmail.com>
2016-10-02 20:33:30 +0300

    Добавил мидлварь для авторизации

--- backend/src/middleware.php
+++ backend/src/middleware.php
@@ -2,3 +2,13 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+//
+$users = [];
+$users[getenv('FIRST_USER')] = getenv('FIRST_USER_PASSWORD');
+
+if(getenv('DISABLE_AUTH') !== 'true'){
+  $app->add(new \Slim\Middleware\HttpBasicAuthentication([
+    "secure" => false,
+    "users" => $users
+  ]));
+}
