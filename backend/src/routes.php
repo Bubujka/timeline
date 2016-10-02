9365e9f78dcb8cd63b021a5cae2c03e31b1687dc
Aleksej Kamynin <zendzirou@gmail.com>
2016-10-02 20:17:22 +0300

    Метод на чтение, запись, поправил knex

--- backend/src/routes.php
+++ backend/src/routes.php
@@ -5,3 +5,31 @@ $app->get('/[{name}]', function ($req, $res, $args) {
   $t = $this->pdo->query('show tables')->fetchAll();
   return $res->withJson($t);
 });
+
+$app->get('/api/events', function ($req, $res, $args) {
+  $data = $this->pdo->query('select `id`, `name`, unix_timestamp(`time`) as `time` from events')->fetchAll();
+  return $res->withJson(array_map(function($itm){
+    $itm['id'] = (int)$itm['id'];
+    $itm['time'] = (int)$itm['time'];
+    $itm['is_user'] = true;
+    return $itm;
+  }, $data));
+});
+
+$app->get('/api/events/types', function ($req, $res, $args) {
+  return $res->withJson($this->pdo->query('select distinct `name` from events')->fetchAll());
+});
+$app->post('/api/events', function ($req, $res, $args) {
+  $b = $req->getParsedBody();
+  $name = $b['name'];
+  if(isset($b['time'])){
+    $time = (int)$b['time'];
+  }else{
+    $time = time();
+  }
+  $this
+    ->pdo
+    ->prepare('insert into events(`name`, `time`, `created_at`) values (?, from_unixtime(?), now())')
+    ->execute([$b['name'], $time]);
+  return $res->withJson(true);
+});
--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,8 @@ const conf =  {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT
+    port: process.env.MYSQL_PORT,
+    charset: 'utf8'
   },
   pool: {
     min: 2,
