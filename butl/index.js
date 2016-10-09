68df6d30708238a530b232bb29d5aa42e7b1b241
Aleksej Kamynin <zendzirou@gmail.com>
2016-10-09 21:13:01 +0300

    Начало работы над клиентом

---
+++ butl/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const request = require('request');
+const chalk = require('chalk');
+const SERVER = 'http://timeline.najomi.org';
+
+program.version('0.0.1');
+program
+  .command('checklogin <user> <pass>')
+  .action(function(user, pass){
+    request
+      .get(SERVER, (err, res) => {
+        if(err){
+          throw err;
+        }
+
+        if(res.statusCode === 200){
+          console.log(chalk.green('Пароль подходит для сервера'));
+        }else{
+          console.log(chalk.red('Что-то ты не угадал +_+'));
+        }
+      })
+      .auth(user, pass);
+  });
+
+
+
+program.parse(process.argv);
