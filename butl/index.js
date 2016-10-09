87062ab04146b5fa472e115e9e16f2f9dbd2bb9a
Aleksej Kamynin <zendzirou@gmail.com>
2016-10-09 21:28:49 +0300

    Сохранение пароля в файл

--- butl/index.js
+++ butl/index.js
@@ -3,7 +3,9 @@
 const program = require('commander');
 const request = require('request');
 const chalk = require('chalk');
+const et = require('expand-tilde');
 const SERVER = 'http://timeline.najomi.org';
+const fs = require('fs');
 
 program.version('0.0.1');
 program
@@ -24,6 +26,18 @@ program
       .auth(user, pass);
   });
 
+program
+  .command('savelogin <user> <pass>')
+  .action(function(user, pass){
+    const pth = et('~/.butl.json');
+
+    fs.writeFile(pth, JSON.stringify({ user, pass}, null, 2), (err) => {
+      if(err){
+        throw err;
+      }
+      console.log(chalk.green(`Пароль сохранён в файл "${pth}"`));
+    });
+  });
 
 
 program.parse(process.argv);
